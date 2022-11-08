ffabc2a913da5c3d3be14b4da3122f405325a6cd
Danielle P <42582307+dn-palmer@users.noreply.github.com>
2022-11-08 11:19:00 -0500

    Version 1
    No identity

---
+++ DnDAPI/DnDAPI/Controllers/DungeonMasterController.cs
@@ -0,0 +1,72 @@
+﻿using DnDAPI.Models.Entities;
+using DnDAPI.Services;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DnDAPI.Controllers
+{
+    //Controler for the Dungeon Master Table. Allows interaction with the data held inside.
+    [EnableCors]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DungeonMasterController : ControllerBase
+    {
+        private readonly IDnDRepository _repos;
+        public DungeonMasterController(IDnDRepository repository)
+        {
+            _repos = repository;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateDM( DungeonMaster dungeonMaster)
+        {
+            _repos.CreateDungeonMasterAsync(dungeonMaster);
+            return Ok();
+        }
+
+
+        [HttpGet("ReadAll")]
+        public async Task<IActionResult> ReadAll()
+        {
+            var dmList = await _repos.ReadAllDMAsync();
+
+            //gets the game prefrence for each entry if it exists.
+            foreach(var d in dmList)
+            {
+                d.GamePreferences = await _repos.ReadPreferencesAsync(d.Id);
+            }
+
+            return Ok(dmList);
+        }
+
+        [HttpGet("Read/{id}")]
+        public async Task<IActionResult> Read(int id)
+        {
+            var dungeonMaster = await _repos.ReadDMAsync(id);
+
+            if(dungeonMaster == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(dungeonMaster);
+
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(DungeonMaster dungeonMaster)
+        {
+            await _repos.UpdateGMAsync(dungeonMaster);
+            return NoContent();
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            _repos.DeleteGMAsync(id);
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ DnDAPI/DnDAPI/Models/Entities/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace DnDAPI.Models.Entities;
+
+//Player entitie to establish a palyer table in the db upon running a migration. 
+public class Player
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public int YearsOfExperiance { get; set; } = 0;
+    public string Email { get; set; } = String.Empty;
+    public GamePreferences? GamePreferences { get; set; }
+    public int? DungeonMasterId { get; set; }
+    public DungeonMaster? DungeonMaster { get; set; }
+
+}
