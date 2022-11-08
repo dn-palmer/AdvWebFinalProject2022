c8dbe9780b1eca5ef2d50111311707533e694a31
Danielle P <42582307+dn-palmer@users.noreply.github.com>
2022-11-08 10:56:03 -0500

    Change of Plans
    This would work better using the Identity API!! My original Entities were more like Roles and granting access based on Role makes so much more sense.

--- DnDAPI/DnDAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DnDAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
