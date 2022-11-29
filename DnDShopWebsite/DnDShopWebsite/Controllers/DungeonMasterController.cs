3839f42b07dc592a83834f6684128c2580e9afa4
Danielle P <42582307+dn-palmer@users.noreply.github.com>
2022-11-29 17:13:36 -0500

    Progress
    Still working on the Views

---
+++ DnDShopWebsite/DnDShopWebsite/Controllers/DungeonMasterController.cs
@@ -0,0 +1,33 @@
+﻿using DnDShopWebsite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DnDShopWebsite.Controllers
+{
+    public class DungeonMasterController : Controller
+    {
+        private readonly IDnDRepository _repo;
+
+        public DungeonMasterController(IDnDRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dungeonMasters = await _repo.ReadAllDMAsync();
+            return View(dungeonMasters);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var dungeonMaster = await _repo.ReadDMAsync(id);
+
+            if(dungeonMaster == null)
+            {
+                return RedirectToAction("Index", "DungeonMaster");
+            }
+
+            return View(dungeonMaster);
+        }
+    }
+}
